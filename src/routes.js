3b7ae54bfa02a4f517104da69f5d65ebefb7afac
LeoGomes0919 <leonardogs0919@gmail.com>
2020-04-06 01:19:53 -0300

    [backend] desenvolvimento das rotas login e filtro de casos por ONG.

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,17 @@ const express = require('express');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
+routes.post('/session', SessionController.create);
+
 routes.get('/list/ongs', OngController.list);
 routes.post('/create/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.list);
 
 routes.post('/create/incidents', IncidentController.create);
 routes.get('/list/incidents', IncidentController.list);
