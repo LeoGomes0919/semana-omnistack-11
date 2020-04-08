3444aeda324ed9242adb87f7428382daccba02fd
LeoGomes0919 <leonardogs0919@gmail.com>
2020-04-07 23:33:42 -0300

    [frontend] Integração do backend com o frontend, e comunição dos metodo HTTP, GET, POST, DELET.

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,32 +1,53 @@
-import React from 'react';
-import {FiLogIn} from 'react-icons/fi';
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import { FiLogIn } from 'react-icons/fi';
+import { Link, useHistory } from 'react-router-dom';
 
+import api from '../../services/api';
 import './style.css';
 
 import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
 
-function Logon(){
-  return(
+function Logon() {
+  const [id, setId] = useState('');
+  const history = useHistory();
+
+  async function hendleLogin(e) {
+    e.preventDefault();
+
+    try {
+      const response = await api.post('session', { id });
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', response.data.name);
+      history.push('/profile')
+    } catch (err) {
+      alert('ID incorreto ou nao existe!')
+    }
+  }
+
+  return (
     <div className="logon-container">
       <section className="form">
-        <img src={logoImg} alt="Be The Hero" title="Be The Hero"/>
-      
-        <form>
+        <img src={logoImg} alt="Be The Hero" title="Be The Hero" />
+
+        <form onSubmit={hendleLogin}>
           <header>
             <h1>Faça seu logon</h1>
           </header>
 
-          <input placeholder="Sua ID"/>
+          <input
+            placeholder="Sua ID"
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
           <button className="button" type="submit">Entrar</button>
           <Link to="/register" className="back-link">
-            <FiLogIn size="16" color="#E02141"/>
+            <FiLogIn size="16" color="#E02141" />
             Não tenho cadastro
           </Link>
         </form>
       </section>
-        <img src={heroesImg} alt="Heros" title="Heros"/>
+      <img src={heroesImg} alt="Heros" title="Heros" />
     </div>
   );
 }
--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
 import { FiArrowLeft } from 'react-icons/fi'
 
+import api from '../../services/api';
 import './style.css';
 import logoImg from '../../assets/logo.svg';
 
 function NewIncident() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+  const ongId = localStorage.getItem('ongId');
+  const history = useHistory();
+
+  async function handleNewIncident(e) {
+    e.preventDefault();
+    const data = {
+      title,
+      description,
+      value,
+    };
+
+    try {
+      await api.post('create/incidents', data, {
+        headers: {
+          Authorization: ongId,
+        }
+      });
+     history.push('/profile');
+    } catch (err) {
+      alert('Erro ao tentar efetuar cadastro.')
+    }
+  }
+
   return (
     <div className="new-incident-container">
       <div className="new-incident-content">
@@ -22,10 +49,22 @@ function NewIncident() {
            </Link>
         </section>
 
-        <form>
-          <input placeholder="Titulo do caso" />
-          <textarea placeholder="Descrição do caso" />
-          <input placeholder="Valor em reais" />
+        <form onSubmit={handleNewIncident}>
+          <input
+            placeholder="Titulo do caso"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+          <textarea
+            placeholder="Descrição do caso"
+            value={description}
+            onChange={e => setDescription(e.target.value)}
+          />
+          <input
+            placeholder="Valor em reais"
+            value={value}
+            onChange={e => setValue(e.target.value)}
+          />
           <button className="button" type="submit">Cadastrar</button>
         </form>
       </div>
